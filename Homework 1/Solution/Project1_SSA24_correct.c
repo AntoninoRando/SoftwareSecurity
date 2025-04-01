6fa936475dcb9f0e10912871337276a94791e4a6
Antonino Rando <rando.1985084@studenti.uniroma1.it>
2025-04-02 00:00:27 +0200

    feat: Add homework 1 correct code

--- Homework 1/Solution/Project1_SSA24_correct.c
+++ Homework 1/Solution/Project1_SSA24_correct.c
@@ -1,54 +1,109 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
 #include <ctype.h>
+#include <cstdio>
 
 void func1(const char *src) {
-    size_t len = strlen(src);
-    char *dst = malloc(len + 1);
-    if (dst) {
-        strncpy(dst, src, len);
-        dst[len] = '\0';
-        free(dst);
-    }
+    if (!src) return; 
+
+    // Limit in case of non null-terminated src
+    size_t n = strnlen(src, 1024);
+    char *dst = (char *)malloc(n + 1);
+
+    if (!dst) return;
+    snprintf(dst, n + 1, "%s", src);
+    free(dst);
 }
 
 void func2(int fd) {
-    char *buf;
     size_t len;
-    if (read(fd, &len, sizeof(len)) != sizeof(len) || len > 1024)
-        return;
-    buf = malloc(len + 1);
-    if (buf) {
-        if (read(fd, buf, len) == len) {
-            buf[len] = '\0';
-        }
+    ssize_t ret = read(fd, &len, sizeof(len));
+    if (ret != sizeof(len)) return;
+    if (len == 0 || len > 1024)  return;
+    
+    char *buf = (char *)malloc(len + 1);
+    if (!buf) return;
+
+    ret = read(fd, buf, len);
+    if (ret != (ssize_t)len) {
         free(buf);
+        return;
     }
+
+    buf[len] = '\0';
+    free(buf);
 }
 
-void func3() {
-    char buffer[1024];
-    printf("Please enter your user id:");
-    if (fgets(buffer, sizeof(buffer), stdin)) {
-        if (!isalpha(buffer[0])) {
-            char errormsg[1044];
-            snprintf(errormsg, sizeof(errormsg), "%s is not a valid ID", buffer);
-        }
+int func3() {
+    size_t buffer_size = 1024;
+    char *buffer = (char *)malloc(buffer_size);
+    if (!buffer) return -1;
+
+    printf("Please enter your user ID: ");
+    if (!fgets(buffer, buffer_size, stdin)) { free(buffer); return -2; }
+
+    size_t len = strnlen(buffer, buffer_size);
+
+    // Aware of trailing new-lines
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
     }
+
+    // Validate that the first character is a letter
+    if (buffer[0] == '\0' || !isalpha((unsigned char)buffer[0])) {
+        // Use a dynamic buffer to avoid fixed-size issues
+        size_t err_len = len + 21; // " is not a valid ID" + 1 (null terminator)
+        char *errormsg = (char *)malloc(err_len);
+        if (!errormsg) {
+            fprintf(stderr, "Memory allocation failed.\n");
+            free(buffer);
+            return -3;
+        }
+
+        snprintf(errormsg, err_len, "%s is not a valid ID", buffer);
+        fprintf(stderr, "%s\n", errormsg);
+        free(errormsg);
+    }
+
+    free(buffer);
+    return 0;
 }
 
 void func4(const char *foo) {
-    char *buffer = (char *)malloc(11);
-    if (buffer) {
-        strncpy(buffer, foo, 10);
-        buffer[10] = '\0';
-        free(buffer);
-    }
+    if (!foo) return;
+
+    size_t len = strnlen(foo, 9);
+    char *buffer = (char *)malloc(10);
+    if (!buffer) return;
+
+    snprintf(buffer, len, "%s", foo);
+    free(buffer);
 }
 
-int main() {
-    func4("safe_input");
-    func3();
+main()
+{
+    int y = 10;
+    int a[10];
+
+    func4("fooooooooooooooooooooooooooooooooooooooooooooooooooo");
+
+    // Use error code instead of try and catch
+    
+    int i = func3();
+    if (i == -1) fprintf(stderr, "...");
+    if (i == -2) fprintf(stderr, "...");
+    if (i == -3) fprintf(stderr, "...");
+    
+    fprintf(aFile, "%s", "hello world");
+
+    while (y > 0)
+    {
+        y = y - 1;
+        a[y] = y;
+    }
+
     return 0;
 }
